7b31ff8e8a7f0a6081287ba6cfb2817ab7fd7ee0
Raullen Chai <raullenchai@gmail.com>
2018-01-05 18:36:31 -0800

    make contract pausible

--- token/IoTeXNetwork.sol
+++ token/IoTeXNetwork.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 
 import './zeppelin/math/SafeMath.sol';
-import './zeppelin/ownership/Ownable.sol';
+import './zeppelin/lifecycle/Pausable.sol';
 import './zeppelin/token/BasicToken.sol';
 import './zeppelin/token/ERC20.sol';
 import './zeppelin/token/ERC20Basic.sol';
 import './zeppelin/token/StandardToken.sol';
 
-contract IoTeXNetwork is StandardToken, Ownable {
+contract IoTeXNetwork is StandardToken, Pausable {
     string  public  constant name = "IoTeX Network";
     string  public  constant symbol = "IOTX";
     uint    public  constant decimals = 18;
@@ -18,28 +18,37 @@ contract IoTeXNetwork is StandardToken, Ownable {
         _;
     }
 
-    function IoTeXNetwork( uint tokenTotalAmount, address admin ) {
+    function IoTeXNetwork( uint tokenTotalAmount) {
         // Mint all tokens. Then disable minting forever.
         balances[msg.sender] = tokenTotalAmount;
         totalSupply = tokenTotalAmount;
         Transfer(address(0x0), msg.sender, tokenTotalAmount);
-
-        transferOwnership(admin); // admin could drain tokens that were sent here by mistake
     }
 
-    function transfer(address _to, uint _value)
+    function transfer(address _to, uint _value) whenNotPaused
         validDestination(_to)
         returns (bool) {
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint _value)
+    function transferFrom(address _from, address _to, uint _value) whenNotPaused
         validDestination(_to)
         returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
 
-    function emergencyERC20Drain( ERC20 token, uint amount ) onlyOwner {
-        token.transfer( owner, amount );
+    function approve(address _spender, uint256 _value) public whenNotPaused
+      returns (bool) {
+      return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenNotPaused
+      returns (bool success) {
+      return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused
+      returns (bool success) {
+      return super.decreaseApproval(_spender, _subtractedValue);
     }
 }
